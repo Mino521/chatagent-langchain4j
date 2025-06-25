217f79be2c2be67fd90e5a6b96b8f9923ca522aa
Zeming Wang <wzm970527@gmail.com>
2025-06-26 00:56:18 +1000

    introduce memory separate

--- src/test/java/com/jamiewang/chatagentlangchain4j/assistant/MemoryAssistantTests.java
+++ src/test/java/com/jamiewang/chatagentlangchain4j/assistant/MemoryAssistantTests.java
@@ -16,4 +16,17 @@ public class MemoryAssistantTests {
         String answer2 = memoryAssistant.chat("Who am I?");
         System.out.println(answer2);
     }
+
+    @Autowired
+    private SeparateChatAssistant separateChatAssistant;
+
+    @Test
+    public void testSeparateChatAssistant() {
+        String answer1 = separateChatAssistant.chat(1, "I am Jamie");
+        System.out.println(answer1);
+        String answer2 = separateChatAssistant.chat(2, "Who am I?");
+        System.out.println(answer2);
+        String answer3 = separateChatAssistant.chat(1, "Who am I?");
+        System.out.println(answer3);
+    }
 }
